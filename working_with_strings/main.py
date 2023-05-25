9d83868431ff70b50d666b88a8832a12b8b7fe03
p-karari <kararikelvin2002@gmail.com>
2023-05-25 14:59:45 +0300

    Working with strings in python

---
+++ working_with_strings/main.py
@@ -0,0 +1,28 @@
+print("Working with strings")
+
+#newline use \n
+
+print("Creating a \n new line")
+
+#printing the value inside of a variable
+example = "Some value"
+print(example)
+
+#concatenation with strings
+
+print(example + "has been concatenated")
+
+#string functions
+
+print(example.upper()) #converting a string to uppercase
+print(example.lower()) #converting a string to lower case
+print(example.isupper()) #checking if a string isuppercase
+print(len(example)) #function to check the length of a string
+print(example[8]) #printing the value at a specif index on a string
+print(example.index("me")) #prints the index pf a specified character(s) in a string
+print(example.replace("Some", "a")) #function to replace a value in a string
+
+
+
+
+
