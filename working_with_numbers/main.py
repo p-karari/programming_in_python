2b408a827ee477b71a5baddff5870007e6ac9c9e
p-karari <kararikelvin2002@gmail.com>
2023-05-25 15:14:15 +0300

    working with numbers in python

---
+++ working_with_numbers/main.py
@@ -0,0 +1,29 @@
+from math import *
+#printing a number
+print(2.85)
+
+#basic arithmetic
+print(3 + 5)
+print(5 * 5)
+print(8 / 4)
+print(8 % 3) #modulus operator
+
+#concatenating a number variable to a string
+my_num = 4
+print(str(my_num) + " is my lucky number")# the str() method is for converting a variable to a string
+
+#math functons and methods associated with numbers
+print(abs(my_num)) #method to get the absolute value of a given number or variable
+print(pow(my_num, 2)) #raise a value to a specified power, first value is a number , second value is the power
+print(pow(8, 6))
+print(max(10, 100, 80)) #compare the given values and ooutputs the maximum
+print(min(250, 52, -8)) #minimum value method
+print(round(3.2)) #roundin numbers
+
+#more complex functions, require you to import a module called math
+#from math import *
+print(floor(8.2)) #gets the floor value of a number
+print(ceil(6.92)) #rounds a value up
+print(sqrt(64)) #square root of a number
+
+
