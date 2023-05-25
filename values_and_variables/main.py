a1c25b29fea36b7ccf622a85c46da2e14aaabbd5
p-karari <kararikelvin2002@gmail.com>
2023-05-25 14:58:46 +0300

    variables and datatypes in python

---
+++ values_and_variables/main.py
@@ -0,0 +1,9 @@
+character_name = "Levine"
+character_age = "50"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old")
+
+character_name = "Empress"
+print("He really liked the name  " + character_name + ",")
+print("but didn't like being  " + character_age + ".")
