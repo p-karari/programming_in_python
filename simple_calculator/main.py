22af93a8b6915de683ef12626d54c10706e28a1b
p-karari <kararikelvin2002@gmail.com>
2023-05-25 15:14:45 +0300

    a simple calculator in python

---
+++ simple_calculator/main.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number : ")
+num2 = input("Enter another number : ")
+
+result = float(num1) + float(num2)
+#int() method converts a value to an integer value
+#float() converts a value to a floating point number
+
+print(result)
