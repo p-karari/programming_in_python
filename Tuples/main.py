87f4fa88aab389620601a1dc425952c61cf3dea2
p-karari <kararikelvin2002@gmail.com>
2023-05-26 22:24:07 +0300

    tuples in python

---
+++ Tuples/main.py
@@ -0,0 +1,8 @@
+#a tuple is a list whose elements are immutable meaning one can not modify elements inside of a tuple
+
+coordinates = (8 ,2)
+print(coordinates)
+
+#list of tuples
+coordinates2 = [(8 , 8) , (56, 40) , (5, 22)]
+print(coordinates2)
