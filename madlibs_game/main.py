210237eab436fab6f5cec569f9a8163996c0a67c
p-karari <kararikelvin2002@gmail.com>
2023-05-26 21:27:01 +0300

    simple madlibs game in python

---
+++ madlibs_game/main.py
@@ -0,0 +1,7 @@
+color = input("Enter a colour : ")
+plural_noun = input("Enter a plural Noun : ")
+celebrity = input("Enter a celebrity's name : ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
