56b2dca36efa6fd0409027ac54e8d8edfdd30925
p-karari <kararikelvin2002@gmail.com>
2023-05-26 21:27:42 +0300

    lists in python

---
+++ lists_in_python/main.py
@@ -0,0 +1,7 @@
+friends = ["June" , "Empress" , "Zendaya" , "Josie"]
+
+print(friends[0]) #accessing a single element in a list by its index
+print(friends[1:5]) #accessing elements in a certain range
+
+friends[2] = "April" #modifying elements of a list
+print(friends[2])
