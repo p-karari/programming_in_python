9a2b3268b3dec980cbc013d6423833444c52d83d
p-karari <kararikelvin2002@gmail.com>
2023-09-19 23:49:15 +0300

    average between two numbers

---
+++ challenge3/main.py
@@ -0,0 +1,10 @@
+# Write a program that:
+# • asks the user to input two numbers
+# • calculates the average
+# • outputs the result
+
+num1 = int(input("Enter first number : "))
+num2 =  int(input("Enter second number : "))
+
+result = (num1 + num2) /2
+print("Result is : ", str(result))
