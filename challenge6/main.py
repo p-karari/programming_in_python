44fbee6ed09895e6e891c836f30780e1663e326a
p-karari <kararikelvin2002@gmail.com>
2023-09-20 09:08:28 +0300

    programme that prompts a user and displays a response

---
+++ challenge6/main.py
@@ -0,0 +1,11 @@
+# Write a program that:
+# • asks the user their name
+# • asks what their favourite subject is
+# (using their name in the question)
+# • responds to their answer by saying
+# that you like that subject as well
+
+name = input("Enter your name : ")
+subject = input("Favourite subject : ")
+
+print("Hello " + name + ", i like " + subject + " as well")
