3d7c2bdb100802353324de612285e9c38517b037
p-karari <kararikelvin2002@gmail.com>
2023-09-20 09:00:19 +0300

    programme that adds two numbers , divides the first by the second and outputs the result

---
+++ challenge5/main.py
@@ -0,0 +1,11 @@
+# Write a program that:
+# • asks the user to input two numbers
+# • divides the first number by the second
+# number
+# • outputs the result
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+result = num1/num2
+
+print("Answer is : ", str(result))
