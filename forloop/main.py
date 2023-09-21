0a1d39d1572cfacb80fa381583718a8c2286b5a7
p-karari <kararikelvin2002@gmail.com>
2023-09-21 13:57:36 +0300

    for loop in python

---
+++ forloop/main.py
@@ -0,0 +1,10 @@
+friends = ["JIm" , "Karen" , "Kelvin"]
+
+for friend in friends: #looping through elements of an array
+    print(friend)
+
+for num in range(10 , 100): #looping through range of values
+    print(num)
+
+for name in range(len(friends)): #looping through an array using range and len() methods
+    print(friends[name])
