38be3f0b6930e48e071a6dce00ca7acd1d100906
p-karari <kararikelvin2002@gmail.com>
2023-09-25 23:38:11 +0300

    Classes and objects in python

---
+++ classesandobjectss/main.py
@@ -0,0 +1,8 @@
+from student import Student
+
+student1 = Student("mike", "Biology", 3.5, False) #student object
+student2 = Student("Kev", "Science", 4.0, False)
+
+
+print(student1.name )
+print(student2.name )
