f9db90ac99388f77f1f138dcc1b406b01f1899e1
p-karari <kararikelvin2002@gmail.com>
2023-09-25 11:29:40 +0300

    Modules in python

---
+++ modulesinpython/additional_tools.py
@@ -0,0 +1,6 @@
+import random
+
+def roll_dice(num) :
+    return random.randint(1, num)
+
+
---
+++ modulesinpython/main.py
@@ -0,0 +1,6 @@
+#modules in python
+#external modules - modules that don't come pre installed in modules
+
+import additional_tools
+
+print(additional_tools.roll_dice(6))
