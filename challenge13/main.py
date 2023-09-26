8fecb8c3295833318a893debc2643ee6720bdd54
p-karari <kararikelvin2002@gmail.com>
2023-09-26 20:48:28 +0300

    string manipulation in python

---
+++ challenge13/main.py
@@ -0,0 +1,16 @@
+# Challenge 13
+# Write a program that:
+# • asks the user to input a sentence
+# • asks the user to input:
+# • the word they want to replace
+# • the word they want to replace it with
+# • outputs the new sentence
+
+sentence  = input("Enter any sentence you would like : ")
+word1 = input("Enter word you wish to replace : ")
+word2 = input("Enter word to replace with : ")
+
+print(sentence.replace(word1, word2))
+
+
+
