dddcbd602fa792709b0c255cf69147a4526a2212
p-karari <kararikelvin2002@gmail.com>
2023-09-26 20:50:50 +0300

    objects and functions in python

--- objectfunctions/Car_details.py
+++ objectfunctions/Car_details.py
@@ -3,7 +3,7 @@ class Car_details:
         self.brand_name = brand_name
         self.model_name = model_name
         self.horse_power = horse_power
-   
+
 
     def is_a_race_car(self):
         if self.horse_power >= 600:
