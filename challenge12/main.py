5363d3d69a7c2a4433cb8c66a7fde978c474dde1
p-karari <kararikelvin2002@gmail.com>
2023-09-26 20:49:41 +0300

    string manipulation challenge

---
+++ challenge12/main.py
@@ -0,0 +1,8 @@
+# Challenge 11
+# Write a program that:
+# • asks the user to input a sentence
+# • calculates and outputs how many
+# characters there are in the sentence
+
+sentence = input("Input a sentence : ")
+print(len(sentence))
