eb88b974c37d93e0884765e7962b41f636d2db1e
p-karari <kararikelvin2002@gmail.com>
2023-09-27 21:39:19 +0300

    python programm to print out odd numbers between 1 and 100

---
+++ challenge17oddnumbers/main.py
@@ -0,0 +1,19 @@
+# Challenge 17
+# Write a program that:
+# • outputs all odd numbers between
+# 1 and 99
+
+for num in range(1, 100):
+    odd = num % 2
+    if odd != 0:
+        print(num)
+
+
+
+
+        
+
+
+
+
+
