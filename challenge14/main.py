04148f7ef2373a4fe6b5513e413f630279f9861a
p-karari <kararikelvin2002@gmail.com>
2023-09-28 23:49:43 +0300

    python challenge series, challenge 14

---
+++ challenge14/main.py
@@ -0,0 +1,7 @@
+# Write a program that:
+# • asks the user to input a sentence
+# • outputs the number of times the word
+# 'the' occurs in it
+
+text = input("Enter your sentence : ")
+print("The appears " , text.count("the") , " times")
